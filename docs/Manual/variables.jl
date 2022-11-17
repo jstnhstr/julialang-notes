6e22e3d14ef47c7dea3b37ef7d1ce7cc70f60749
Justin Hester <justinlawrencehester@gmail.com>
2022-11-16 21:38:27 -0800

    Added variables.ipynb

---
+++ docs/Manual/variables.jl
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Julia 1.8.2
+#     language: julia
+#     name: julia-1.8
+# ---
+
+# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[]
+# # Variables
+# -
+
+x = 10
+
+x + 1
+
+x = 1 + 1
+
+x = "Hello World!"
+
+μ = 52.04
+
+baybayin = "ᜊᜊᜌᜒ"
+
+ᜊᜊᜌᜒ = "baybayin"
+
+ᜊᜊᜌᜒ ^ 2
+
+α² = 3.6
+
+?δ
+
+pi
+
+pi = 3
+
+sqrt = 4
+
+sqrt(100)
+
+# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[]
+# ## Allowed Variable Names
+# -
+
+x, ___ = size([2 2; 1 1])
+
+___
+
+y = ___
+
+else = false
+
+try = "No"
+
+🧃 = "juice"
+
+"apple" * 🧃
+
+# + [markdown] jp-MarkdownHeadingCollapsed=true tags=[]
+# ## Stylistic Conventions
+# -
+
+variable = "v"
+
+only_underscore_for_readability = true
+
+EnglishConsonant = Type{string}
+
+perform = rand
+
+perform()
+
+append! = push!
+
+list = [1,2]
+
+append!(list, 3)
+
+list
